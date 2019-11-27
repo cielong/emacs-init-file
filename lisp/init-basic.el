29a23d7ee2fdd7245b34da1a8520d333e332f749
cielong <yutian.wu@mail.com>
2019-11-27 04:40:54 -0800

    Minor improve - set backup directory to ".~" on all platform

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -2,16 +2,16 @@
 (setq is-mac (equal system-type 'darwin))
 (when is-mac
   (require-package 'exec-path-from-shell)
-  (exec-path-from-shell-initialize)
-  ;; Set Back-up directory & version control
-  (setq
-   backup-by-copying t
-   backup-directory-alist
-    '(("." . ".~"))
-   delete-old-versions t
-   kept-new-versions 6
-   kept-old-versions 2
-   version-control t))
+  (exec-path-from-shell-initialize))
+
+;; Set Back-up directory & version control on all platform
+(setq
+ backup-by-copying t
+ backup-directory-alist'(("." . ".~"))
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t)
 
 ;; Using company mode for all the mode
 (setq ispell-program-name "/usr/local/bin/ispell")
@@ -28,11 +28,13 @@
 (add-hook 'after-init-hook 'yas-global-mode)
 
 ;; Turn on dired-omit-mode
+;; Reset dired-omit-fiels to only files begin with '.'
+;; excluding special directory '.' and '..'
 (require 'dired-x)
 (setq-default dired-omit-files-p t) ; Buffer-local variable
 (setq dired-omit-files "^\\.[^.]+$")
 
-;; load extra file-pattern that wants to be ignored
+;; Load extra file pattern that wants to be omitted
 (defvar dired-omit-files-config "~/.emacs.d/.dired-omit-files")
 
 (when (file-exists-p dired-omit-files-config)
@@ -44,12 +46,12 @@
 		(setq dired-omit-files (concat dired-omit-files file-pattern)))
 	      (split-string (buffer-string) "\n" t)))))
 
-;; sort dir first in dired
+;; Sort directory first in dired
 (require 'ls-lisp)
 (setq ls-lisp-dirs-first t)
 (setq ls-lisp-use-insert-directory-program nil)
 
-;; toggle dired-sidebar by default
+;; Toggle dired-sidebar
 (use-package dired-sidebar
   :bind (("C-c f" . dired-sidebar-toggle-sidebar))
   :ensure t
@@ -68,7 +70,7 @@
   (setq dired-sidebar-use-term-integration t)
   (setq dired-sidebar-use-custom-font t))
 
-;; toggle ibuffer-sidebar by default
+;; Toggle ibuffer-sidebar by default
 (use-package ibuffer-sidebar
   :bind (("C-c b" . ibuffer-sidebar-toggle-sidebar))
   :ensure t
