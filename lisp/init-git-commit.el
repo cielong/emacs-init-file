6bd0e6e7fec2fd98eb0cbfb648a43a45578a1e3a
cielong <yutian.wu@mail.com>
2020-02-05 21:10:13 -0800

    git: get the basename of the branch name in case of path-like naming convention

--- lisp/init-git-commit.el
+++ lisp/init-git-commit.el
@@ -2,9 +2,10 @@
   (when (and (stringp buffer-file-name)
 	     (string-match "\.git\/COMMIT_EDITMSG$" buffer-file-name))
     (setq BRANCH_NAME
-	  (substring
-	   (shell-command-to-string "git rev-parse --abbrev-ref HEAD")
-	   0 -1))
+	  (file-name-nondirectory
+	   (substring
+	    (shell-command-to-string "git rev-parse --abbrev-ref HEAD")
+	    0 -1)))
     (replace-string "${BRANCH_NAME}" BRANCH_NAME)))
 (add-hook 'find-file-hook 'expand-commit-message)
 (provide init-git-commit)
